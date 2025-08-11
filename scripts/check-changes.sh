9356d8a6dec408ac199e53653e58a26f2fd11198
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-11 15:20:26 +0600

    build scripts updated

--- scripts/check-changes.sh
+++ scripts/check-changes.sh
@@ -2,7 +2,9 @@
 
 if git diff --quiet $VERCEL_GIT_PREVIOUS_SHA $VERCEL_GIT_COMMIT_SHA -- packages/create-servest; then
   echo "No changes in packages/create-servest, skipping build"
-  exit 0
+  exit 100  # special exit code indicating skip
 else
   echo "Changes detected in packages/create-servest, proceeding with build"
+  exit 0
 fi
+
