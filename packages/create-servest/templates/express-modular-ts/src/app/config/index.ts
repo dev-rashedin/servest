7c0c637b0f56bbeb311899930cd35e505826016c
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-12 00:05:01 +0600

    express-modular-ts template added

---
+++ packages/create-servest/templates/express-modular-ts/src/app/config/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface Config {
+  port: number;
+  dbUrl?: string;
+  jwtSecret?: string;
+}
+
+const config: Config = {
+  port: Number(process.env.PORT) || 3000,
+  dbUrl: process.env.DB_URL,
+  jwtSecret: process.env.JWT_SECRET,
+};
+
+
+export default config;
