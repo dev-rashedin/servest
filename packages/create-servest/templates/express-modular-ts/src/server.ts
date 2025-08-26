2a0ec9d4edfa14396b1525d7bcbe4b220549469e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-26 20:20:37 +0600

    templates structure updated for create-servest

--- packages/create-servest/templates/express-modular-ts/src/server.ts
+++ packages/create-servest/templates/express-modular-ts/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
-import config from './app/config';
+import config from './config';
 
 app.listen(config.port, () => {
   console.log(`Server listening on port http://localhost:${config.port}`);
