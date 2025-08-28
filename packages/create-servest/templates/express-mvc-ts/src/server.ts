77ff7d53eaa181f5920bf682b4d47e1754601950
Rashedin-063 <rashedinislam.06@gmail.com>
2025-08-28 13:58:52 +0600

    create-servest templates folder returned to old src/app structure

--- packages/create-servest/templates/express-mvc-ts/src/server.ts
+++ packages/create-servest/templates/express-mvc-ts/src/server.ts
@@ -1,5 +1,5 @@
 import app from './app';
-import config from './config';
+import config from './app/config';
 
 app.listen(config.port, () => {
   console.log(`Server listening on port http://localhost:${config.port}`);
