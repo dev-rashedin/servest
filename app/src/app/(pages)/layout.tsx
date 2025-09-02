cd899cc253985184b23c2806ba9a991aef2576cc
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-02 14:53:37 +0600

    docs layout created

--- app/src/app/(pages)/layout.tsx
+++ app/src/app/(pages)/layout.tsx
@@ -1,31 +1,17 @@
-import type { Metadata } from 'next';
-import React, { JSX } from 'react';
-import styles from './layout.module.css';
+import './docs.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 
-export const metadata: Metadata = {
-  title: 'Servest – The Ultimate Backend Starter & Addon Toolkit',
-  description:
-    'Servest is an open-source monorepo to create ready-made backend templates for Express, Django, Laravel, Fastify and more. It also allows you to add features like ESLint, Prettier, Mongoose, Prisma and more with a single click.',
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>): JSX.Element {
+export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={styles.layout}>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <div className="">
-            <div className="min-h-[calc(100vh-192px)] boundary py-8">{children}</div>
-          </div>
+          {children}
         </ThemeProvider>
       </body>
     </html>
