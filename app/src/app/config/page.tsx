aa4d4660de4c979a28fe3adecaa8280db5fddb2a
Rashedin-063 <rashedinislam.06@gmail.com>
2025-09-02 09:42:04 +0600

    config/index.mdx created and displayed

--- app/src/app/config/page.tsx
+++ app/src/app/config/page.tsx
@@ -1,6 +1,17 @@
+// app/config/page.tsx
+import { promises as fs } from 'fs';
+import path from 'path';
+import { compileMDX } from 'next-mdx-remote/rsc';
 import { JSX } from 'react';
 
-const page = (): JSX.Element => {
-  return <div>page</div>;
-};
-export default page;
+export default async function ConfigPage(): Promise<JSX.Element> {
+  const filePath = path.join(process.cwd(), '../docs/config/index.mdx');
+  const source = await fs.readFile(filePath, 'utf-8');
+
+  const { content } = await compileMDX({
+    source,
+    options: { parseFrontmatter: true },
+  });
+
+  return <div className="prose prose-lg dark:prose-invert mx-auto">{content}</div>;
+}
