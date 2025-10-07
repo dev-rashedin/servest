82c50d73ffee3c45b4b2d63ae1110150f33dc52e
Rashedin-063 <rashedinislam.06@gmail.com>
2025-10-07 15:36:53 +0600

    mongoose.mdx added

--- app/src/data/index.ts
+++ app/src/data/index.ts
@@ -11,7 +11,7 @@ export const navItems = [
 ];
 
 export const docsOrder: Record<string, string[]> = {
-  addons: ['index', '_Addons', 'f-fileName', 'eslint', 'prettier', 'eslint-Prettier'],
+  addons: ['index', '_Addons', 'f-fileName', 'eslint', 'prettier', 'eslint-Prettier', 'mongoose'],
   guide: [
     'index',
     'philosophy',
