ff86168f2d8de0757d392113c65fb84e371d9e5a
Rashedin-063 <rashedinislam.06@gmail.com>
2025-10-24 16:30:25 +0600

    loadingSkeleton updated, small, medium screen template docs visibility issue fixed Closes #56

--- app/src/components/ui/Home/LoadingSkeleton.tsx
+++ app/src/components/ui/Home/LoadingSkeleton.tsx
@@ -3,11 +3,14 @@ const LoadingSkeleton = () => {
     <div className="scroll-container animate-pulse">
       <div className="scroll-content wall">
         {Array.from({ length: 1 }).map((_, idx) => (
-          <div key={idx} className="space-y-4 min-h-[75vh] flex flex-col justify-center ">
-            <div className="h-12 w-4/6 bg-muted/60 rounded-lg " />
-            <div className="h-20 w-5/6 bg-muted/60 rounded-lg mb-1" />
-            <div className="h-40 w-5/6 bg-muted/60 rounded-lg mb-1" />
-            <div className="h-12 w-3/4 bg-muted/60 rounded-lg" />
+          <div
+            key={idx}
+            className="space-y-4 min-h-[75vh] flex flex-col justify-center px-4 md:px-8"
+          >
+            <div className="h-12 md:w-11/12 lg:w-4/6 bg-muted/60 rounded-lg " />
+            <div className="h-20  lg:w-5/6 bg-muted/60 rounded-lg mb-1" />
+            <div className="h-40  lg:w-5/6 bg-muted/60 rounded-lg mb-1" />
+            <div className="h-12 md:w-10/12 lg:w-3/4 bg-muted/60 rounded-lg" />
           </div>
         ))}
       </div>
