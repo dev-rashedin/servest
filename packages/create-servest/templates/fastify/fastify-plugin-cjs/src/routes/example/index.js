9dbf85cc746bfe10f8fc44f786728ad11bb91e1a
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-03 12:39:03 +0600

    fix: update example route (fastify-plugin-cjs, fastify-plugin-esm)

--- packages/create-servest/templates/fastify/fastify-plugin-cjs/src/routes/example/index.js
+++ packages/create-servest/templates/fastify/fastify-plugin-cjs/src/routes/example/index.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-toolkit');
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/example', async function (request, reply) {
+  fastify.get('/', async function (request, reply) {
     return {
       success: true,
       message: 'This is an example route',
--- packages/create-servest/templates/fastify/fastify-plugin-esm/src/routes/example/index.js
+++ packages/create-servest/templates/fastify/fastify-plugin-esm/src/routes/example/index.js
@@ -1,5 +1,5 @@
 export default async function (fastify, opts) {
-  fastify.get('/example', async function (request, reply) {
+  fastify.get('/', async function (request, reply) {
     return {
       success: true,
       message: 'This is an example route',
