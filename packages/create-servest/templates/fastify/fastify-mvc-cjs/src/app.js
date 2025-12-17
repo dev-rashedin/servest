6ccf73253db3756334c0e16c81eca092bb1e1c21
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-17 18:52:17 +0600

    feat: add not found, global error handler(fastify-mvc-cjs)

--- packages/create-servest/templates/fastify/fastify-mvc-cjs/src/app.js
+++ packages/create-servest/templates/fastify/fastify-mvc-cjs/src/app.js
@@ -11,10 +11,42 @@ app.register(cors);
 
 // home route
 app.get('/', async (request, reply) => {
-  reply.code(StatusCodes.OK).send({
+  reply.code(StatusCodes.OK);
+  return {
     success: true,
     message: 'Server is running',
-  });
+  };
+});
+
+// not found handler
+app.setNotFoundHandler((request, reply) => {
+  reply.code(StatusCodes.NOT_FOUND);
+  return {
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: request.url,
+        message: 'API Not Found',
+      },
+    ],
+  };
+});
+
+// global error handler
+app.setErrorHandler((error, request, reply) => {
+  const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  reply.code(statusCode);
+  return {
+    success: false,
+    message: error.message || 'Internal Server Error',
+    errorMessages: [
+      {
+        path: request.url,
+        message: error.message || 'Internal Server Error',
+      },
+    ],
+  };
 });
 
 module.exports = app;
