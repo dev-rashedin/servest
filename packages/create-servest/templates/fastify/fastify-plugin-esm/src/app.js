0a8742ddde0ea3de7ead9d32817799742b2d5faa
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-17 19:21:33 +0600

    feat: add not found, global error handler(fastify-plugin-esm)

--- packages/create-servest/templates/fastify/fastify-plugin-esm/src/app.js
+++ packages/create-servest/templates/fastify/fastify-plugin-esm/src/app.js
@@ -1,6 +1,7 @@
 import AutoLoad from '@fastify/autoload';
 import cors from '@fastify/cors';
 import sensible from '@fastify/sensible';
+import { StatusCodes } from 'http-status-toolkit';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -13,6 +14,37 @@ export default async function (fastify, opts) {
   // Register plugins
   fastify.register(cors);
   fastify.register(sensible);
+
+  // not found handler
+  fastify.setNotFoundHandler((request, reply) => {
+    reply.code(StatusCodes.NOT_FOUND);
+    return {
+      success: false,
+      message: 'Not Found',
+      errorMessages: [
+        {
+          path: request.url,
+          message: 'API Not Found',
+        },
+      ],
+    };
+  });
+
+  // global error handler
+  fastify.setErrorHandler((error, request, reply) => {
+    const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+    reply.code(statusCode);
+    return {
+      success: false,
+      message: error.message || 'Internal Server Error',
+      errorMessages: [
+        {
+          path: request.url,
+          message: error.message || 'Internal Server Error',
+        },
+      ],
+    };
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
