a8aa7e204872848e022114dafdedd89fbe3c12f6
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-17 19:17:06 +0600

    feat: add not found, global error handler(fastify-plugin-cjs)

--- packages/create-servest/templates/fastify/fastify-plugin-cjs/src/app.js
+++ packages/create-servest/templates/fastify/fastify-plugin-cjs/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const AutoLoad = require('@fastify/autoload');
 const cors = require('@fastify/cors');
 const sensible = require('@fastify/sensible');
+const { StatusCodes } = require('http-status-toolkit');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -9,6 +10,37 @@ module.exports = async function (fastify, opts) {
   // Register plugins
   fastify.register(cors);
   fastify.register(sensible);
+
+  // not found handler
+  fastify.setNotFoundHandler((request, reply) => {
+    reply.code(StatusCodes.NOT_FOUND);
+    return {
+      success: false,
+      message: 'Not Found',
+      errorMessages: [
+        {
+          path: request.url,
+          message: 'API Not Found',
+        },
+      ],
+    };
+  });
+
+  // global error handler
+  fastify.setErrorHandler((error, request, reply) => {
+    const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+    reply.code(statusCode);
+    return {
+      success: false,
+      message: error.message || 'Internal Server Error',
+      errorMessages: [
+        {
+          path: request.url,
+          message: error.message || 'Internal Server Error',
+        },
+      ],
+    };
+  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
