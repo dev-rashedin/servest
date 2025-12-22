6247db903ced042c6767974b3dc7af5e94e1f704
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-22 22:06:09 +0600

    fix: update template(fastify-plugin-cjs)

--- packages/create-servest/templates/fastify/fastify-plugin-cjs/src/app.js
+++ packages/create-servest/templates/fastify/fastify-plugin-cjs/src/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const AutoLoad = require('@fastify/autoload');
 const cors = require('@fastify/cors');
 const sensible = require('@fastify/sensible');
-const { StatusCodes } = require('http-status-toolkit');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -10,37 +9,6 @@ module.exports = async function (fastify, opts) {
   // Register plugins
   fastify.register(cors);
   fastify.register(sensible);
-
-  // not found handler
-  fastify.setNotFoundHandler((request, reply) => {
-    reply.code(StatusCodes.NOT_FOUND);
-    return {
-      success: false,
-      message: 'Not Found',
-      errorMessages: [
-        {
-          path: request.url,
-          message: 'API Not Found',
-        },
-      ],
-    };
-  });
-
-  // global error handler
-  fastify.setErrorHandler((error, request, reply) => {
-    const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-    reply.code(statusCode);
-    return {
-      success: false,
-      message: error.message || 'Internal Server Error',
-      errorMessages: [
-        {
-          path: request.url,
-          message: error.message || 'Internal Server Error',
-        },
-      ],
-    };
-  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
@@ -57,5 +25,3 @@ module.exports = async function (fastify, opts) {
     options: Object.assign({}, opts),
   });
 };
-
-export default app;
