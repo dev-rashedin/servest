e0c01b46a45876f8b176d8d8d6ec590d761e7785
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-22 22:06:25 +0600

    fix: update template(fastify-plugin-esm)

--- packages/create-servest/templates/fastify/fastify-plugin-esm/src/app.js
+++ packages/create-servest/templates/fastify/fastify-plugin-esm/src/app.js
@@ -1,7 +1,6 @@
 import AutoLoad from '@fastify/autoload';
 import cors from '@fastify/cors';
 import sensible from '@fastify/sensible';
-import { StatusCodes } from 'http-status-toolkit';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -14,37 +13,6 @@ export default async function (fastify, opts) {
   // Register plugins
   fastify.register(cors);
   fastify.register(sensible);
-
-  // not found handler
-  fastify.setNotFoundHandler((request, reply) => {
-    reply.code(StatusCodes.NOT_FOUND);
-    return {
-      success: false,
-      message: 'Not Found',
-      errorMessages: [
-        {
-          path: request.url,
-          message: 'API Not Found',
-        },
-      ],
-    };
-  });
-
-  // global error handler
-  fastify.setErrorHandler((error, request, reply) => {
-    const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-    reply.code(statusCode);
-    return {
-      success: false,
-      message: error.message || 'Internal Server Error',
-      errorMessages: [
-        {
-          path: request.url,
-          message: error.message || 'Internal Server Error',
-        },
-      ],
-    };
-  });
 
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
@@ -61,5 +29,3 @@ export default async function (fastify, opts) {
     options: Object.assign({}, opts),
   });
 }
-
-export default app;
