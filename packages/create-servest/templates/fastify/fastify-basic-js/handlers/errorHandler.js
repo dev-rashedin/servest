c237a4656991455779b3c796d053ecf7f79892cc
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-12-22 22:03:52 +0600

    fix: update template(fastify-basic-js)

---
+++ packages/create-servest/templates/fastify/fastify-basic-js/handlers/errorHandler.js
@@ -0,0 +1,32 @@
+const { StatusCodes } = require('http-status-toolkit');
+
+const notFoundHandler = (request, reply) => {
+  reply.code(StatusCodes.NOT_FOUND);
+  return {
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: request.url,
+        message: 'API Not Found',
+      },
+    ],
+  };
+};
+
+const globalErrorHandler = (error, request, reply) => {
+  const statusCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  reply.code(statusCode);
+  return {
+    success: false,
+    message: error.message || 'Internal Server Error',
+    errorMessages: [
+      {
+        path: request.url,
+        message: error.message || 'Internal Server Error',
+      },
+    ],
+  };
+};
+
+module.exports = { notFoundHandler, globalErrorHandler };
