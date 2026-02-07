fa0e0dc2338ca9726f0e05dece1bb19852cf38c9
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2026-02-07 16:18:12 +0600

    docs: add documentation (fastify-plugin-cjs)

--- app/src/data/index.ts
+++ app/src/data/index.ts
@@ -100,7 +100,7 @@ export const nestedDocsOrder: Record<string, { label: string; items: string[] }[
     },
     {
       label: 'Fastify',
-      items: ['fastify-basic-js', 'fastify-mvc-cjs', 'fastify-mvc-esm'],
+      items: ['fastify-basic-js', 'fastify-mvc-cjs', 'fastify-mvc-esm', 'fastify-plugin-cjs'],
     },
     // {
     //   label: 'Django',
